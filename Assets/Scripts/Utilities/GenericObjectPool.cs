8a40490cc8c321898879d350562469f22cc3b442
Andrei Dzemidovich <cityfoggy@mail.ru>
2023-09-04 16:26:15 +0200

    add generic for object pooling

---
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -0,0 +1,51 @@
+﻿using CosmicCuration.Bullets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CosmicCuration.Bullets.BulletPool;
+
+namespace CosmicCuration.Utilities
+{
+    public class GenericObjectPool<T> where T : class
+    {
+        private List<PooledItem<T>> pooledItems = new List<PooledItem<T>>();
+
+        protected T GetItem()
+        {
+            if (pooledItems.Count > 0)
+            {
+                PooledItem<T> item = pooledItems.Find(item => !item.isUsed);
+                if (item != null)
+                {
+                    item.isUsed = true;
+                    return item.Item;
+                }
+            }
+            return CreateNewPooledItem();
+        }
+        private T CreateNewPooledItem()
+        {
+            PooledItem<T> newItem = new PooledItem<T>();
+            newItem.Item = CreateItem();
+            newItem.isUsed = true;
+            pooledItems.Add(newItem);
+            return newItem.Item;
+        }
+
+        protected virtual T CreateItem() => throw new NotImplementedException("Child class dont have implementation of CreateItem()");
+
+        public void ReturnItem(T item)
+        {
+            PooledItem<T> pooledItem = pooledItems.Find(i => i.Item.Equals(item));
+            pooledItem.isUsed = false;
+        }
+
+        public class PooledItem<T>
+        {
+            public T Item;
+            public bool isUsed;
+        }
+    }
+}
